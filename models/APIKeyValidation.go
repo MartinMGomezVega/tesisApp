bc99612d2e11fba95e233fd952216f6552c691f2
MartinMGomezVega <martin_gomezvega@hotmail.com>
2023-05-25 22:59:27 -0300

    Avances con la validacion de la key

--- models/APIKeyValidation.go
+++ models/APIKeyValidation.go
@@ -2,7 +2,7 @@ package models
 
 // APIKeyValidationRequest: Key de la API de Open AI
 type APIKeyValidationRequest struct {
-	APIKey string `json:"api_key"`
+	APIKey string `json:"apikey"`
 }
 
 // APIKeyValidationResponse: True o False, si es valida o no la key
--- models/gptRequest.go
+++ models/gptRequest.go
@@ -2,6 +2,6 @@ package models
 
 // ChatGPTRequest: formato de como es el request para obtener datos desde la API de OpenAI
 type ChatGPTRequest struct {
-	APIKey   string `json:"api_key"`
+	APIKey   string `json:"apikey"`
 	Question string `json:"question"`
 }
